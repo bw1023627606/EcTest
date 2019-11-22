d91dc92609b23f8052bade98dd7678627641f8cc
baiwei <1023627606@qq.com>
2019-11-22 10:13:13 +0800

    测试

---
+++ src/main/java/com/bw/dao/Test.java
@@ -0,0 +1,19 @@
+package com.bw.dao;
+
+/**
+ * @author BAI
+ * @ClassName Test
+ * @date 2019/11/22 10:12
+ */
+public class Test {
+
+    public void aa(){
+
+    }
+
+    public void bb(){
+
+    }
+
+
+}
